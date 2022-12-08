c642cb66b1f2be375357fe8e4a89c18f8dd575ad
alesancor1 <alesancor1@alum.us.es>
2022-12-08 16:35:48 +0100

    feat: Added feature toggles
    This feature is part of Framework setup (#1)

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ import express from "express";
 import { initialize, use } from "@oas-tools/core";
 import { OASSwagger } from "./middleware/oas-swagger.js";
 
-const deploy = async () => {
+const deploy = async (env) => {
     const serverPort = process.env.PORT ?? 8080;
     const app = express();
     app.use(express.json({limit: '50mb'}));
     
+    // Feature toggles
+    let config = {}
+    if (env === "production") {
+        config.middleware = { 
+            validator: { disable: true } // Done in gateway
+        }
+    }
+
+    // Initialize OAS Tools
     use(OASSwagger, {path: "/docs"});
     initialize(app).then(() => {
         http.createServer(app).listen(serverPort, () => {
