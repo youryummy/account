3a164e26bfd9a341393d6adb7efc86ef5c77b5b4
alesancor1 <alesancor1@alum.us.es>
2023-01-14 02:39:03 +0100

    style: lint fix

--- utils/commons.js
+++ utils/commons.js
@@ -15,13 +15,28 @@ export async function signToken(req, res, payload) {
     const oldToken = req.headers.cookie?.match(/(?<=authToken=)[^;\s]*/)?.[0];
     const data = _.merge(oldToken ? _.pick(jwt.decode(oldToken), tokenAttr) : {}, _.pick(payload, tokenAttr));
     
-    //Add attributes from other services to data
+    /* Add attributes from other services to data */
     data.userId = data.username;
-    data.ingredientsIds = await CircuitBreaker.getBreaker(axios, res, {nameOverride: "ingredients", onlyOpenOnInternalError: true}).fire("get", `http://youryummy-ingredients-service/api/v1/ingredients?created_by=${data.username}`).then((res) => res.data.result?.map((ing) => ing._id)).catch((err) => {logger.warn(err.message); return []});
-    data.recipebookIds = await CircuitBreaker.getBreaker(axios, res, {nameOverride: "recipebooks", onlyOpenOnInternalError: true}).fire("get", `http://youryummy-recipesbook-service/api/v1/recipesbooks/findByUserId/${data.username}`).then((res) => res.data.map((rb) => rb._id)).catch((err) => {logger.warn(err.message); return []});
-    data.ratingIds = await CircuitBreaker.getBreaker(axios, res, {nameOverride: "ratings", onlyOpenOnInternalError: true}).fire("get", `http://youryummy-ratings-service/api/v1/ratings/findByUserId/${data.username}`).then((res) => res.data.map((rating) => rating._id)).catch((err) => {logger.warn(err.message); return []});
-    data.eventIds = await CircuitBreaker.getBreaker(axios, res, {nameOverride: "planner", onlyOpenOnInternalError: true}).fire("get", `http://planner/api/v1/events`).then((res) => res.data.filter((event) => event.account === data.username).map((event) => event._id)).catch((err) => {logger.warn(err.message); return []});
-    data.recipeIds = await CircuitBreaker.getBreaker(axios, res, {nameOverride: "recipes", onlyOpenOnInternalError: true}).fire("get", `http://recipes/api/v1/recipes`).then((res) => res.data.filter((recipe) => recipe.userId === data.username).map((recipe) => recipe._id)).catch((err) => {logger.warn(err.message); return []});
+    data.ingredientsIds = await CircuitBreaker.getBreaker(axios, res, {nameOverride: "ingredients", onlyOpenOnInternalError: true}).fire("get", `http://youryummy-ingredients-service/api/v1/ingredients?created_by=${data.username}`).then((res) => res.data.result?.map((ing) => ing._id)).catch((err) => {
+        logger.warn(err.message);
+        return [] 
+    });
+    data.recipebookIds = await CircuitBreaker.getBreaker(axios, res, {nameOverride: "recipebooks", onlyOpenOnInternalError: true}).fire("get", `http://youryummy-recipesbook-service/api/v1/recipesbooks/findByUserId/${data.username}`).then((res) => res.data.map((rb) => rb._id)).catch((err) => {
+        logger.warn(err.message);
+        return [] 
+    });
+    data.ratingIds = await CircuitBreaker.getBreaker(axios, res, {nameOverride: "ratings", onlyOpenOnInternalError: true}).fire("get", `http://youryummy-ratings-service/api/v1/ratings/findByUserId/${data.username}`).then((res) => res.data.map((rating) => rating._id)).catch((err) => {
+        logger.warn(err.message);
+        return [] 
+    });
+    data.eventIds = await CircuitBreaker.getBreaker(axios, res, {nameOverride: "planner", onlyOpenOnInternalError: true}).fire("get", `http://planner/api/v1/events`).then((res) => res.data.filter((event) => event.account === data.username).map((event) => event._id)).catch((err) => {
+        logger.warn(err.message);
+        return [] 
+    });
+    data.recipeIds = await CircuitBreaker.getBreaker(axios, res, {nameOverride: "recipes", onlyOpenOnInternalError: true}).fire("get", `http://recipes/api/v1/recipes`).then((res) => res.data.filter((recipe) => recipe.userId === data.username).map((recipe) => recipe._id)).catch((err) => {
+        logger.warn(err.message);
+        return [] 
+    });
 
     const newToken = jwt.sign(data, secret, {issuer: issuer, expiresIn: '24h' });
     
